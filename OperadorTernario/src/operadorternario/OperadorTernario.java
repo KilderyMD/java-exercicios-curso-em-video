f0e490543884e7f5b60c5a1c9b768e2e3ab377d2
Kildery Dornelas <145910472+KilderyMD@users.noreply.github.com>
2026-03-16 11:58:44 -0300

    Add files via upload

---
+++ OperadorTernario/src/operadorternario/OperadorTernario.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package operadorternario;
+
+/**
+ *
+ * @author KilderyMD
+ */
+public class OperadorTernario {
+
+  
+    public static void main(String[] args) {
+        int r, n1, n2;
+        
+        n1 = 4;
+        n2 = 8;
+        r = (n1>n2) ? 0 : 1;
+        
+        System.out.println(r);
+    }
+    
+}
