92a3cd699011cfe60cc40cca101207c53f1aaad5
Kildery Dornelas <145910472+KilderyMD@users.noreply.github.com>
2026-03-16 11:59:29 -0300

    Add files via upload

---
+++ SuperCalculadora/src/supercalculadora/SuperCalculadora.java
@@ -0,0 +1,17 @@
+
+package supercalculadora;
+
+/**
+ *
+ * @author kilde
+ */
+public class SuperCalculadora {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
